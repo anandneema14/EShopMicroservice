f47d6ea26d40872aea113252733846fa462657cd
Anand Neema <anand.neema14@gmail.com>
2024-12-26 19:04:15 +0530

    Register and Configure the Marten in Program.cs and appsettings.json

--- src/Services/Catalog/Catalog.API/Program.cs
+++ src/Services/Catalog/Catalog.API/Program.cs
@@ -1,3 +1,5 @@
+using JasperFx.Core;
+
 var builder = WebApplication.CreateBuilder(args);
 
 //Add Services to the container
@@ -10,6 +12,13 @@ builder.Services.AddMediatR(configuration =>
     //it tells to MedialtR where to find our Command and query handler classes
     configuration.RegisterServicesFromAssembly(typeof(Program).Assembly);
 });
+builder.Services.AddMarten(options =>
+{
+    options.Connection(builder.Configuration.GetConnectionString("DefaultConnection"));
+    //below line will create DB objects automatically. Not suitable for PROD environments
+    //options.AutoCreateSchemaObjects = true;
+}).UseLightweightSessions();
+
 var app = builder.Build();
 
 //Configure HTTP request pipeline
