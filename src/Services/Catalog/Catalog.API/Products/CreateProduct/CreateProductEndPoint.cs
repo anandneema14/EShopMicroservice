b7e0cddf1e0fbe2c49f6940162d019fd48b09409
Anand Neema <anand.neema14@gmail.com>
2024-12-31 22:30:45 +0530

    Updating Carter references

--- src/Services/Catalog/Catalog.API/Products/CreateProduct/CreateProductEndPoint.cs
+++ src/Services/Catalog/Catalog.API/Products/CreateProduct/CreateProductEndPoint.cs
@@ -1,28 +1,41 @@
 namespace Catalog.API.Products.CreateProduct;
 
-public record CreateProductRequest(string Name, List<string> Category, string Description, string ImageFile, decimal Price);
+public record CreateProductRequest(
+    string Name,
+    List<string> Category,
+    string Description,
+    string ImageFile,
+    decimal Price);
 
 public record CreateProductResponse(Guid Id);
 
-public class CreateProductEndPoint : ICarterModule 
+public class CreateProductEndPoint : ICarterModule
 {
     public void AddRoutes(IEndpointRouteBuilder app)
     {
-        app.MapPost("/products",
-                async (CreateProductRequest request, ISender sender) =>
-                {
-                    //Adapt method is from Mapster package
-                    //Mediator needs command object in order to trigger commandhandler
-                    var command = request.Adapt<CreateProductCommand>();
+        try
+        {
+            app.MapPost("/products",
+                    async (CreateProductRequest request, ISender sender) =>
+                    {
+                        //Adapt method is from Mapster package
+                        //Mediator needs command object in order to trigger commandhandler
+                        var command = request.Adapt<CreateProductCommand>();
 
-                    var result = await sender.Send(command);
+                        var result = await sender.Send(command);
 
-                    var response = result.Adapt<CreateProductResponse>();
-                    return Results.Created($"/products/{response.Id}", response);
-                }).WithName("CreateProduct")
-            .Produces<CreateProductResponse>(StatusCodes.Status201Created)
-            .ProducesProblem(statusCode: StatusCodes.Status400BadRequest)
-            .WithSummary("Create Product")
-            .WithDescription("Create Product");
+                        var response = result.Adapt<CreateProductResponse>();
+                        return Results.Created($"/products/{response.Id}", response);
+                    }).WithName("CreateProduct")
+                .Produces<CreateProductResponse>(StatusCodes.Status201Created)
+                .ProducesProblem(statusCode: StatusCodes.Status400BadRequest)
+                .WithSummary("Create Product")
+                .WithDescription("Create Product");
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine(e);
+            throw;
+        }
     }
 }
--- src/Services/Catalog/Catalog.API/obj/Debug/net8.0/Catalog.API.AssemblyInfo.cs
+++ src/Services/Catalog/Catalog.API/obj/Debug/net8.0/Catalog.API.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Catalog.API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9eb906b758ca93bdd100eb16a14bb698e17b3492")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+252d7738b1499cedf37f297b1b70b8c531878758")]
 [assembly: System.Reflection.AssemblyProductAttribute("Catalog.API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Catalog.API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
